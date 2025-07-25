61af85067dcd5ae8629a3da813297f2d688cfd7b
DobDoiDongDang <66070225@kmitl.ac.th>
2025-07-25 07:40:16 +0700

    doing paramiko thing but it didnt work ;-;

--- paramikolab.py
+++ paramikolab.py
@@ -1,13 +1,9 @@
 import paramiko
 import time
+import os
 
 ssh = paramiko.SSHClient()
-
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-ssh.connect('172.31.38.4', username='admin', key_filename='"C:\Users\darks\Desktop\KEY\admin.ppk"')
-
-stdin, stdout, stderr = ssh.exec_command('ls')
-
-print stdout.readlines()
+""" pkey=key, look_for_keys=True """
+ssh.connect(hostname='172.31.38.1', username='admin', key_filename="C:\\Users\\darks\\Desktop\\KEY\\opensshadmin",allow_agent=False,look_for_keys=False)
 ssh.close()
